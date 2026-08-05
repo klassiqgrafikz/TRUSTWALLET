07de50572ff17eea2053eab94f94c67d2cf24647
klassiqgrafikz <klassiqgrafikz@gmail.com>
2026-08-05 13:05:56 +0100

    Remove 'One Platform, Millions of Assets' homepage section (heading, chain search, network table, stats)

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 async function init(){
   if (typeof maintenanceInit === 'function') maintenanceInit();
   window.addEventListener('scroll',function(){$('header').classList.toggle('scrolled',window.scrollY>10);});
-  populateChainTable();
   initAnimations();
   document.querySelectorAll('.scramble-text').forEach(function(el){scrambleText(el);});
   fetchLivePrices();
